86aece27857500a51e4c24438e90d5d1918a95a6
israel <israel.carvalho@b2wdigital.com>
2022-02-24 14:52:51 -0300

    add style to EmptyList on List component

--- apps/front/components/List/styles.ts
+++ apps/front/components/List/styles.ts
@@ -3,9 +3,12 @@ import styled from 'styled-components'
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
+    height: 100%;
 `
 
-export const Title = styled.h1``
+export const Title = styled.h1`
+    text-align: center;
+`
 
 export const ItemsContainer = styled.ul`
     display: grid;
@@ -13,4 +16,17 @@ export const ItemsContainer = styled.ul`
 
 export const Item = styled.li``
 
-export const EmptyList = styled.p``
+export const EmptyList = styled.p`
+    flex: 0.8;
+    align-self: center;
+
+    width: 80%;
+    margin: auto 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: ${({ theme: { colors } }) => colors.greyLight30};
+    box-shadow: ${({ theme: { colors } }) => `0 0 8px 0 ${colors.greyLight10}`};
+`
