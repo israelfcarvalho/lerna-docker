794e30f619e5b2541ee1aa37aa26de96eb6d0a11
Israel Façanha <israel.facanha@gmail.com>
2022-11-04 07:05:37 -0300

    add description to docker container

--- apps/front/pages/docker/file.tsx
+++ apps/front/pages/docker/file.tsx
@@ -6,45 +6,52 @@ type DockerFileAnnotations = 'FROM' | 'WORKDIR' | 'RUN' | 'CMD'
 
 interface DockerFileProps {
     annotations: Annotations<DockerFileAnnotations>
+    description: Array<string>
 }
 
-const DockerFile: NextPage<DockerFileProps> = ({ annotations }) => {
-    return <AnnotationsComponent title="Docker File" annotations={annotations} />
+const DockerFile: NextPage<DockerFileProps> = ({ annotations, description }) => {
+    return <AnnotationsComponent title="Docker File" annotations={annotations} description={description} />
 }
 
 export const getStaticProps: GetStaticProps<DockerFileProps> = () => {
-    return { props: { annotations } }
+    return { props }
 }
 
 export default DockerFile
 
-const annotations: DockerFileProps['annotations'] = {
-    FROM: {
-        command: 'FROM',
-        description: 'The container you want to start from',
-        example: {
-            text: 'FROM node:16-alpine',
+const props: DockerFileProps = {
+    description: [
+        'A Dockerfile is simply a text-based file with no file extension.',
+        ' A Dockerfile contains a script of instructions that Docker uses to create a container image',
+    ],
+    annotations: {
+        FROM: {
+            command: 'FROM',
+            description: 'The container you want to start from',
+            example: {
+                text: 'FROM node:16-alpine',
+            },
         },
-    },
-    WORKDIR: {
-        command: 'WORKDIR',
-        description: 'The root directory where the application will stand',
-        example: {
-            text: 'WORKDIR /app',
+        WORKDIR: {
+            command: 'WORKDIR',
+            description: 'The root directory where the application will stand',
+            example: {
+                text: 'WORKDIR /app',
+            },
         },
-    },
-    RUN: {
-        command: 'RUN',
-        description: 'The command to run from the WORKDIR',
-        example: {
-            text: 'RUN npm i',
+        RUN: {
+            command: 'RUN',
+            description: 'The command to run from the WORKDIR',
+            example: {
+                text: 'RUN npm i',
+            },
         },
-    },
-    CMD: {
-        command: 'CMD',
-        description: 'The default command to run when starting a container from an image',
-        example: {
-            text: 'CMD ["npm", "run", "dev"]',
+        CMD: {
+            command: 'CMD',
+            description: 'The default command to run when starting a container from an image',
+            example: {
+                text: 'CMD ["npm", "run", "dev"]',
+            },
         },
     },
 }
