207d6a8d9a848ada6d448ec9e793c30376fb7d4d
Israel Façanha <israel.facanha@gmail.com>
2022-11-07 08:52:37 -0300

    Add image annotations

--- apps/front/pages/docker/image.tsx
+++ apps/front/pages/docker/image.tsx
@@ -2,7 +2,7 @@ import { NextPage, GetStaticProps } from 'next'
 import React from 'react'
 import AnnotationsComponent from '../../components/Annotations'
 
-type DockerImageAnnotations = 'build'
+type DockerImageAnnotations = 'build' | 'push' | 'tag'
 
 interface DockerImageProps {
     annotations: Annotations<DockerImageAnnotations>
@@ -19,7 +19,10 @@ export const getStaticProps: GetStaticProps<DockerImageProps> = () => {
 
 const props: DockerImageProps = {
     description: [
-        'When running a container, it uses an isolated filesystem. This custom filesystem is provided by a container image. Since the image contains the container’s filesystem, it must contain everything needed to run an application - all dependencies, configurations, scripts, binaries, etc. The image also contains other configuration for the container, such as environment variables, a default command to run, and other metadata.',
+        'When running a container, it uses an isolated filesystem.',
+        'This custom filesystem is provided by a container image.',
+        'Since the image contains the container’s filesystem, it must contain everything needed to run an application - all dependencies, configurations, scripts, binaries, etc.',
+        'The image also contains other configuration for the container, such as environment variables, a default command to run, and other metadata.',
     ],
     annotations: {
         build: {
@@ -32,6 +35,23 @@ const props: DockerImageProps = {
                     {
                         command: '.',
                         text: 'the relative path to Dockerfile, that contain the instructions to mount the image',
+                    },
+                ],
+            },
+        },
+        push: {
+            command: 'push',
+            description: 'push a image to docker hub registry by default',
+            example: { text: 'docker push repository-name/image-name:tag-name' },
+        },
+        tag: {
+            command: 'tag',
+            description: 'used to give a new name to an image',
+            example: {
+                text: 'docker tag image-name new-image-name',
+                observations: [
+                    {
+                        text: 'is usefull to add a prefix of your repository name, to push it. Ex: docker tag image-name repository-name/image-name',
                     },
                 ],
             },
