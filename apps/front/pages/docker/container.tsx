f9500ed8be53d6eace4fcec1a0ce78007f7dbe17
Israel Façanha <israel.facanha@gmail.com>
2022-11-07 08:52:06 -0300

    Add container topics

--- apps/front/pages/docker/container.tsx
+++ apps/front/pages/docker/container.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps, NextPage } from 'next'
 import AnnotationsComponent from '../../components/Annotations'
 
-type DockerContainerAnnotations = 'run'
+type DockerContainerAnnotations = 'run' | 'ps' | 'stop' | 'rm'
 
 interface DockerContainerProps {
     annotations: Annotations<DockerContainerAnnotations>
@@ -23,6 +23,40 @@ const props: DockerContainerProps = {
         run: {
             command: 'run',
             description: 'start a container',
+            example: {
+                text: 'docker run -p XXXX:YYYY image-name',
+                observations: [
+                    { command: '-p', text: 'this flag is used to map local port XXXX to container port YYYY' },
+                ],
+            },
+        },
+        ps: {
+            command: 'ps',
+            description: 'show a list of containers',
+            example: {
+                text: 'docker ps -a',
+                observations: [{ text: 'this tag for to show all containers (running and stoped)', command: '-a' }],
+            },
+        },
+        stop: {
+            command: 'stop',
+            description: 'stop a running container',
+            example: {
+                text: 'docker stop <container-id | container-name>',
+            },
+        },
+        rm: {
+            command: 'rm',
+            description: 'remove a container',
+            example: {
+                text: 'docker rm -f <container-id | container-name>',
+                observations: [
+                    {
+                        command: '-f',
+                        text: 'this flag forces a running container to be stoped before remove, without it, a running container can not be removed',
+                    },
+                ],
+            },
         },
     },
     description: [
