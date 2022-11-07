5049706197d290f8bf268426d6902db698f915fd
Israel Façanha <israel.facanha@gmail.com>
2022-11-07 09:20:07 -0300

    Create Volume page

---
+++ apps/front/pages/docker/volume/index.tsx
@@ -0,0 +1,24 @@
+import { GetStaticProps, NextPage } from 'next'
+import AnnotationsComponent from '../../../components/Annotations'
+
+interface DockerVolumeProps {
+    description: Array<string>
+}
+
+const DockerVolume: NextPage<DockerVolumeProps> = ({ description }) => {
+    return <AnnotationsComponent title="Docker Volume" description={description} annotations={{}} />
+}
+
+export const getStaticProps: GetStaticProps<DockerVolumeProps> = () => {
+    return { props }
+}
+
+export default DockerVolume
+
+const props: DockerVolumeProps = {
+    description: [
+        'Provide the ability to connect specific filesystem paths of the container back to the host machine',
+        'If a directory in a container is mouted, changes in that directory are also seen on the host machine',
+        'If we mount that same directory across container restarts, we-de see the same files',
+    ],
+}
