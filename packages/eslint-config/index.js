a49f7b9882a86ed190ca798b18cdc4a4c9795adc
Israel Façanha <israel.facanha@gmail.com>
2024-01-24 23:31:41 -0300

    Upgrade eslint-config prettier settings

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,22 +2,22 @@
 const prettierConfig = require('./.prettierrc.json')
 
 module.exports = {
-    root: true,
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-        'plugin:json/recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    env: {
-        es6: true,
-        node: true,
-    },
-    plugins: ['prettier', '@typescript-eslint'],
-    rules: {
-        'no-undef': 'off',
-        'no-unused-vars': 'error',
-        'prettier/prettier': ['error', prettierConfig],
-        '@typescript-eslint/padding-line-between-statements': 'error',
-    },
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:json/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'no-undef': 'off',
+    'no-unused-vars': 'error',
+    'prettier/prettier': ['error', prettierConfig],
+    '@typescript-eslint/padding-line-between-statements': 'error',
+  },
 }
