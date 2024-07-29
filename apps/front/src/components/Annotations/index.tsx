7eef0058ec6ee8e6861e15f9882ef09e59701778
Israel <israel.facanha@gmail.com>
2024-07-29 02:56:44 -0300

    update files with linter

--- apps/front/src/components/Annotations/index.tsx
+++ apps/front/src/components/Annotations/index.tsx
@@ -1,120 +1,94 @@
 import React, { useMemo } from 'react'
 import {
-        Container,
-        ItemsContainer,
-        Title,
-        EmptyList,
-        Item,
-        Explanation,
-        ExplanationCommand,
-        ExplanationDescription,
-        Example,
-        ExampleText,
-        ExampleObservations,
-        Observation,
-        ObservationCommand,
-        ObservationText,
-        DescriptionContainer,
-        Description,
+  Container,
+  ItemsContainer,
+  Title,
+  EmptyList,
+  Item,
+  Explanation,
+  ExplanationCommand,
+  ExplanationDescription,
+  Example,
+  ExampleText,
+  ExampleObservations,
+  Observation,
+  ObservationCommand,
+  ObservationText,
+  DescriptionContainer,
+  Description,
 } from './styles'
-import {} from '@facanha/eslint-config'
 
 type AnnotationsItem = PossibleValues<Annotations<any>>
 
 interface AnnotationsData {
-        hasAnnotations: boolean
-        items: Array<AnnotationsItem>
+  hasAnnotations: boolean
+  items: Array<AnnotationsItem>
 }
 
 interface AnnotationsComponentProps {
-        title?: string
-        description?: Array<string>
-        annotations: Annotations<any>
-        emptyListMessage?: string
+  title?: string
+  description?: Array<string>
+  annotations: Annotations<any>
+  emptyListMessage?: string
 }
 
 const AnnotationsComponent: React.FC<AnnotationsComponentProps> = ({
-        annotations,
-        title,
-        emptyListMessage = 'No content',
-        description = [],
+  annotations,
+  title,
+  emptyListMessage = 'No content',
+  description = [],
 }) => {
-        const { hasAnnotations, items } = useMemo<AnnotationsData>(() => {
-                const annotationsItems = Object.keys(annotations).map(annotationKey => annotations[annotationKey])
-                const hasAnnotations = !!annotationsItems.length
+  const { hasAnnotations, items } = useMemo<AnnotationsData>(() => {
+    const annotationsItems = Object.keys(annotations).map(annotationKey => annotations[annotationKey])
+    const hasAnnotations = !!annotationsItems.length
 
-                return {
-                        hasAnnotations,
-                        items: annotationsItems,
-                }
-        }, [annotations])
+    return {
+      hasAnnotations,
+      items: annotationsItems,
+    }
+  }, [annotations])
 
-        return (
-                <Container>
-                        {!!title && <Title>{title}</Title>}
-                        {!!description.length && (
-                                <DescriptionContainer>
-                                        {description.map((paragraph, index) => (
-                                                <Description key={index}>{paragraph}</Description>
-                                        ))}
-                                </DescriptionContainer>
-                        )}
-                        {hasAnnotations ? (
-                                <ItemsContainer>
-                                        {items.map((item, itemIndex) => (
-                                                <Item key={itemIndex}>
-                                                        <Explanation>
-                                                                <ExplanationCommand>
-                                                                        {item.command}:{' '}
-                                                                </ExplanationCommand>
-                                                                <ExplanationDescription>
-                                                                        {item.description}
-                                                                </ExplanationDescription>
-                                                        </Explanation>
-                                                        {!!item.example && (
-                                                                <Example>
-                                                                        <ExampleText>{item.example.text}</ExampleText>
-                                                                        {!!item.example.observations && (
-                                                                                <ExampleObservations>
-                                                                                        {item.example.observations.map(
-                                                                                                (
-                                                                                                        observation,
-                                                                                                        index
-                                                                                                ) => (
-                                                                                                        <Observation
-                                                                                                                key={
-                                                                                                                        index
-                                                                                                                }
-                                                                                                        >
-                                                                                                                {!!observation.command && (
-                                                                                                                        <ObservationCommand>
-                                                                                                                                {
-                                                                                                                                        observation.command
-                                                                                                                                }
-
-                                                                                                                                :{' '}
-                                                                                                                        </ObservationCommand>
-                                                                                                                )}
-                                                                                                                <ObservationText>
-                                                                                                                        {
-                                                                                                                                observation.text
-                                                                                                                        }
-                                                                                                                </ObservationText>
-                                                                                                        </Observation>
-                                                                                                )
-                                                                                        )}
-                                                                                </ExampleObservations>
-                                                                        )}
-                                                                </Example>
-                                                        )}
-                                                </Item>
-                                        ))}
-                                </ItemsContainer>
-                        ) : (
-                                <EmptyList>{emptyListMessage}</EmptyList>
-                        )}
-                </Container>
-        )
+  return (
+    <Container>
+      {!!title && <Title>{title}</Title>}
+      {!!description.length && (
+        <DescriptionContainer>
+          {description.map((paragraph, index) => (
+            <Description key={index}>{paragraph}</Description>
+          ))}
+        </DescriptionContainer>
+      )}
+      {hasAnnotations ? (
+        <ItemsContainer>
+          {items.map((item, itemIndex) => (
+            <Item key={itemIndex}>
+              <Explanation>
+                <ExplanationCommand>{item.command}: </ExplanationCommand>
+                <ExplanationDescription>{item.description}</ExplanationDescription>
+              </Explanation>
+              {!!item.example && (
+                <Example>
+                  <ExampleText>{item.example.text}</ExampleText>
+                  {!!item.example.observations && (
+                    <ExampleObservations>
+                      {item.example.observations.map((observation, index) => (
+                        <Observation key={index}>
+                          {!!observation.command && <ObservationCommand>{observation.command}: </ObservationCommand>}
+                          <ObservationText>{observation.text}</ObservationText>
+                        </Observation>
+                      ))}
+                    </ExampleObservations>
+                  )}
+                </Example>
+              )}
+            </Item>
+          ))}
+        </ItemsContainer>
+      ) : (
+        <EmptyList>{emptyListMessage}</EmptyList>
+      )}
+    </Container>
+  )
 }
 
 export default AnnotationsComponent
--- apps/front/src/pages/_app.tsx
+++ apps/front/src/pages/_app.tsx
@@ -4,33 +4,33 @@ import PageWrapper from '../components/PageWraper'
 import { GlobalStyle } from '../styles/Global.styles'
 
 const theme: DefaultTheme = {
-    spacing: {
-        tiny: '2px',
-        mini: '4px',
-        small: '8px',
-        medium: '16px',
-        large: '32px',
-        huge: '64px',
-    },
-    colors: {
-        primary: '#2e2547',
-        grey: '#757575',
-        greyLight10: '#a9a9a9',
-        greyLight20: '#c9c9c9',
-        greyLight30: '#efefef',
-        blue: '#3944bc',
-    },
+  spacing: {
+    tiny: '2px',
+    mini: '4px',
+    small: '8px',
+    medium: '16px',
+    large: '32px',
+    huge: '64px',
+  },
+  colors: {
+    primary: '#2e2547',
+    grey: '#757575',
+    greyLight10: '#a9a9a9',
+    greyLight20: '#c9c9c9',
+    greyLight30: '#efefef',
+    blue: '#3944bc',
+  },
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <PageWrapper>
-                <Component {...pageProps} />
-            </PageWrapper>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <PageWrapper>
+        <Component {...pageProps} />
+      </PageWrapper>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
